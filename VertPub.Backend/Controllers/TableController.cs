a1c19ef2fce60cb4dd03928802310297adebe571
simbasang <sebastian.englund@live.se>
2020-12-17 13:21:38 +0100

    repo and controller for table

---
+++ VertPub.Backend/Controllers/TableController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VertPub.Backend.Models;
+using VertPub.Backend.Repos;
+
+namespace VertPub.Backend.Controllers
+{
+
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class TableController : ControllerBase
+    {
+        private readonly ITableRepo _repo;
+        public TableController(ITableRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TableModel>>> GetAllTables()
+        {
+            var result = await _repo.GetAllTables();
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateTable([FromBody] TableModel table)
+        {
+            var result = await _repo.CreateTable(table);
+            return Ok(result);
+        }
+    }
+}
+
+
+
+
