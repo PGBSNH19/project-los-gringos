54ce369ccb7f16eda299702be192aae09447f004
simbasang <sebastian.englund@live.se>
2020-12-29 13:02:41 +0100

    added list and call to db

--- VirtPub/Pages/PrivateTable.cshtml.cs
+++ VirtPub/Pages/PrivateTable.cshtml.cs
@@ -5,21 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-
+using Microsoft.Extensions.Logging;
+using VirtPub.Models;
+using VirtPub.Services;
 
 namespace VirtPub.Pages
 {
     [Authorize]
     public class PrivateTableModel : PageModel
     {
+        private readonly GameService _service;
+        public List<GameLinksModel> Games = new List<GameLinksModel>();
+
+        public PrivateTableModel(GameService service)
+        {
+            _service = service;
+        }
+
         [BindProperty(SupportsGet =true)]
         public Guid Id { get; set; }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet()
         {
             if(Id== Guid.Empty)
                 return RedirectToPage("/Index");
+
+            Games = await _service.GetGames();
             return Page();
         }
 
