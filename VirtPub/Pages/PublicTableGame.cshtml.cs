1dd306d646736fed0ce3d570320aa4f5c0fbcd95
Adgnascor <william.l.dev@gmail.com>
2020-12-30 13:17:52 +0100

    Show table Id

--- VirtPub/Pages/PublicTableGame.cshtml.cs
+++ VirtPub/Pages/PublicTableGame.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using VirtPub.Models;
@@ -15,18 +16,29 @@ namespace VirtPub.Pages
     public class PublicTableGameModel : PageModel
     {
         private readonly ILogger<PublicTableModel> _logger;
-        private readonly GameService _service;
-        public TableModel Table = new TableModel();
 
+        private readonly GameService _service;
+        public TableModel Table {get; set;}
+        public GameLinksModel Game {get; set;} = new GameLinksModel();
+
+        [BindProperty(SupportsGet= true)]
+        public Dictionary<string,string> SelectedTable {get; set;}
+        
+
+        [BindProperty(SupportsGet= true)]
+        public Dictionary<string,string> SelectedGame {get; set;}
         public PublicTableGameModel(ILogger<PublicTableModel> logger, GameService service)
         {
             _logger = logger;
             _service = service;
         }
         
-        public async Task OnGet(Dictionary<string, string> selectedTable)
+        public async Task OnGet()
         {
-            Table = await _service.GetTableById(selectedTable["id"]);
+            Table = await _service.GetTableById(SelectedTable["id"]);
+            var games = await _service.GetGames();
+
+            Game = games.Where(x=> x.id == Table.game.id).FirstOrDefault();
         }
     }
 }
