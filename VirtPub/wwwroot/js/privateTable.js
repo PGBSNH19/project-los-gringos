6f94ed460c7829a73452e5200b63dc533617b19e
simbasang <sebastian.englund@live.se>
2021-01-04 11:46:41 +0100

    fix buggs

--- VirtPub/wwwroot/js/privateTable.js
+++ VirtPub/wwwroot/js/privateTable.js
@@ -2,15 +2,15 @@
 
 var privateTableConnection = new signalR.HubConnectionBuilder().withUrl("/privatetablehub").build();
 
-privateTableConnection.on("UpdateUserList", function () {
+privateTableConnection.on("UpdateUserList", ()=> {
     $('#UserListPartial').load('/PrivateTable?handler=UserListPartial&tableId=' + Group);
 });
 
-privateTableConnection.start().then(function () {
+privateTableConnection.start().then(() =>{
     privateTableConnection.invoke("AddUserToUserList", Group);
     $('#UserListPartial').load('/PrivateTable?handler=UserListPartial&tableId=' + Group);
 });
 
-window.addEventListener("unload", function(event) { 
+window.addEventListener("unload", (event) =>{ 
     privateTableConnection.invoke("RemoveUserFromUserList", Group);
 });
